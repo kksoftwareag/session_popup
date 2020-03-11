c1efc86b41fced812abbdfa7752b62c86ed43557
Karina Kern <karina@webschmiede.at>
2020-03-11 12:01:22 +0100

    Added flexform to plugin

--- ext_tables.php
+++ ext_tables.php
@@ -15,3 +15,11 @@ call_user_func(
 
     }
 );
+
+// Add flexforms for frontend plugin
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+
+$pluginSignature = strtolower($extensionName) . '_sessionpopup';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/SessionpopupPlugin.xml');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature]='layout,select_key,recursive';
