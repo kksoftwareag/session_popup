27d5b9f1c5d350f65f896c398a23ccbbfe3e1f1d
Karina Kern <karina@webschmiede.at>
2020-03-12 11:23:56 +0100

    Show only once per session (optional)

--- Classes/Controller/PopupController.php
+++ Classes/Controller/PopupController.php
@@ -1,39 +1,24 @@
 <?php
 namespace Webschmiede\SessionPopup\Controller;
 
-/***************************************************************
- *  Copyright notice
+/***
  *
- *  (c) 2020 Karina Kern <karina@webschmiede.at>, Webschmiede GmbH
+ * This file is part of the "session_popup" Extension for TYPO3 CMS.
  *
- *  All rights reserved
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * (c) 2020 Karina Kern <karina@webschmiede.at>, Webschmiede GmbH
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ ***/
 
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
-/**
- * @package session_popup
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- */
 class PopupController extends ActionController
 {
     /**
+     * Show Session Popup
+     *
      * @return void
      */
     public function showAction()
@@ -59,6 +44,44 @@ class PopupController extends ActionController
             $viewAssign['ce'] = $contentObject;
         }
 
+        // show only once per session (if activated in FlexForm)
+        if (!$this->settings['session'] || !$this->alreadyShown()) {
+            $showPopup = true;
+            $viewAssign['show-popup'] = $showPopup;
+            // store in session
+            if ($this->settings['session']) {
+                $sessionVars = $this->generateSessionData();
+            }
+        }
+
         $this->view->assignMultiple($viewAssign);
     }
+
+    /**
+     * @return void
+     */
+    protected function generateSessionData() {
+
+        $sessionVars = $GLOBALS['TSFE']->fe_user->getKey('ses','session_popup');
+        $sessionVars['shown'] = 1;
+        $GLOBALS['TSFE']->fe_user->setKey('ses', 'session_popup', $sessionVars);
+        $GLOBALS['TSFE']->storeSessionData();
+    }
+
+    /**
+     * @return boolean
+     */
+    protected function alreadyShown() {
+        // do not check if session storage is disabled in FlexForm
+        if (!$this->settings['session']) return false;
+
+        // get session data
+        $sessionData = $GLOBALS['TSFE']->fe_user->getKey('ses','session_popup');
+
+        // already shown?
+        if ($sessionData['shown'] == 1) return true;
+
+    }
+
 }
+
