4447958b1203cb72bfb0f526e89a0f83952aaf93
Bernhard Sirlinger <sirlinger@kk-software.de>
2021-10-14 18:14:15 +0200

    🎉 Initial Commit

---
+++ Classes/Controller/PopupController.php
@@ -0,0 +1,112 @@
+<?php
+namespace Webschmiede\SessionPopup\Controller;
+
+/***
+ *
+ * This file is part of the "session_popup" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) 2020 Karina Kern <karina@webschmiede.at>, Webschmiede GmbH
+ *
+ ***/
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class PopupController extends ActionController
+{
+    /**
+     * Show Session Popup
+     *
+     * @return void
+     */
+    public function showAction()
+    {
+        #$GLOBALS['TSFE']->fe_user->removeSessionData();
+
+        $viewAssign = array();
+
+        // image
+        if ($this->settings['input'] == 'image') {
+            $resourceFactory = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance();
+            $fileReference = $resourceFactory->getFileReferenceObject($this->settings['image']);
+            $viewAssign['image'] = $fileReference;
+        }
+
+        // content element
+        if ($this->settings['input'] == 'ce') {
+            $cObj = $this->configurationManager->getContentObject();
+            $cObjConf = array(
+                'tables' => 'tt_content',
+                'source' => $this->settings['ce'],
+                'dontCheckPid' => 1
+            );
+            $contentObject = $cObj->cObjGetSingle('RECORDS', $cObjConf);
+            $viewAssign['ce'] = $contentObject;
+        }
+
+        // show only once per session (if activated in FlexForm)
+        if (!$this->settings['session'] || !$this->alreadyShown()) {
+            $showPopup = true;
+            $viewAssign['show-popup'] = $showPopup;
+            // store in session
+            if ($this->settings['session']) {
+                $sessionVars = $this->generateSessionData();
+            }
+        }
+
+        $this->view->assignMultiple($viewAssign);
+    }
+
+    /**
+     * @return void
+     */
+    protected function generateSessionData() {
+
+        $sessionVars = $GLOBALS['TSFE']->fe_user->getKey('ses','session_popup');
+
+        switch ($this->settings['sessiontype']) {
+            case 'global': // GENERAL
+                $sessionVars['global'] = 1;
+                break;
+            case 'page': // PAGE ID
+                $sessionVars['page'][$GLOBALS['TSFE']->id] = 1;
+                break;
+            case 'ce': // CE UID
+            default:
+                $sessionVars['ce'][$this->configurationManager->getContentObject()->data['uid']] = 1;
+                break;
+        }
+
+        $GLOBALS['TSFE']->fe_user->setKey('ses', 'session_popup', $sessionVars);
+        $GLOBALS['TSFE']->storeSessionData();
+    }
+
+    /**
+     * @return boolean
+     */
+    protected function alreadyShown() {
+        // do not check if session storage is disabled in FlexForm
+        if (!$this->settings['session']) return false;
+
+        // get session data
+        $sessionData = $GLOBALS['TSFE']->fe_user->getKey('ses','session_popup');
+
+        // already shown?
+        switch($this->settings['sessiontype']) {
+            case 'global': // GENERAL
+                if ($sessionData['global'] == 1) return true;
+                break;
+            case 'page': // PAGE ID
+                if ($sessionData['page'][$GLOBALS['TSFE']->id]==1) return true;
+                break;
+            case 'ce': // CE UID
+                if ($sessionData['ce'][$this->configurationManager->getContentObject()->data['uid']] = 1) return true;
+                break;
+        }
+
+    }
+
+}
+
---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Session Popup',
+    'description' => 'Configure a popup that appears once a session.',
+    'category' => 'plugin',
+    'author' => 'Bernhard Sirlinger',
+    'author_email' => 'sirlinger@kk-software.de',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '3.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
